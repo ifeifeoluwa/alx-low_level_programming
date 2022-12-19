eee52f73574f7bca4910ca9c1ce8d4815282a320
Ife <ife.ifeoluwa@gmail.com>
2022-12-18 19:23:03 -0800

    printing using putc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,15 @@
 
 /**
  * main - Entry point
- * Prints on screen
- * Return - Always 0 (Success)
+ *
+ * This program prints on screen
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char str[80] =  "\"Programming is like building a multilingual puzzle\"";
+	char str[80] =  "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
