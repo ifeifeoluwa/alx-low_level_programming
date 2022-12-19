457c25550af8ac012467507650d89cf528255785
Ife <ife.ifeoluwa@gmail.com>
2022-12-18 19:38:55 -0800

    using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * this program prints a string and a new lime to the screen
+ *
+ * Result: always 0 (success)
+ */
+
+int main(void)
+{
+	char str[89] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(str);
+
+	return (0);
+}
