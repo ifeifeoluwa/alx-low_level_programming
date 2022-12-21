dd85f01dd1750f47fdcd7ae8f015aedd03544a4f
Ife <ife.ifeoluwa@gmail.com>
2022-12-20 16:15:00 -0800

    string reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,28 @@
-:x
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * rev_string - reverses a string and prints it on the screen
+ * @s: pointer variable
+ */
 
+void rev_string(char *s)
+{
+	int i, sz, hf = 0;
+	char ft, lt;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	sz = i - 1;
+	hf = sz / 2;
+	while (hf >= 0)
+	{
+		ft = s[sz - hf];
+		lt = s[hf];
+		s[hf] = ft;
+		s[sz - hf] = lt;
+		hf--;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string and a new line
+ * @str: pointer variables
+ */
+
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	while (count >= 0
+
+	{
+	}
+
+}
