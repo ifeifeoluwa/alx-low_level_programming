ae82df1ea352a8c87f4c8593418fa81828a5f633
Ife <ife.ifeoluwa@gmail.com>
2022-12-20 18:43:02 -0800

    advanced stuffs

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer variable
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	unsigned int ct = 0, sz = 0, j = 0, k = 1, m = 1, i;
+
+	while (*(s + ct) != '\0')
+	{
+		if (sz > 0 && (*(s + ct) < '0' || *(s + ct) > '9'))
+			break;
+
+		if (*(s + ct) == '-')
+			k *= -1;
+
+		if ((*(s + ct) >= '0') && (*(s + ct) <= '9'))
+		{
+			if (sz > 0)
+				m *= 10;
+			size++
+		}
+		ct++;
+	}
+	for (i = ct - sz; i < ct; i++)
+	{
+		j = j + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (j * k);
+}
