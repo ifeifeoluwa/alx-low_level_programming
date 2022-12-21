3f65c656e19aa038e23bd71970f90be5a289fa38
Ife <ife.ifeoluwa@gmail.com>
2022-12-20 17:16:31 -0800

    string half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,19 @@ void puts_half(char *str)
 {
 	int i, count = 0;
 
-	while (count >= 0
-
+	while (count >= 0)
 	{
+		if (str[count] == '\0')
+			break;
+		count++;
 	}
 
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
