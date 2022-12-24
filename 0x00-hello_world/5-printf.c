618950550eadd32d603b2e002656273491881743
Ife <ife.ifeoluwa@gmail.com>
2022-12-24 03:11:33 -0800

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * this program prints a string and a new lime to the screen
+ * main - Entry point
  *
  * Result: always 0 (success)
  */
 
 int main(void)
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+{
+	printf("with proper grammar, but the out
+come is a piece of art,\n");
 	return (0);
 }
