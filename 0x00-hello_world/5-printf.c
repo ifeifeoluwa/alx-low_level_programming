5144bb08a5a01f124d26156da6dfa91a0521e0de
Ife <ife.ifeoluwa@gmail.com>
2022-12-24 03:21:51 -0800

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point
  *
- * Result: always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the out
-come is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
