4e703269825d66aece850d65ce4244b78e3e0f2b
Ife <ife.ifeoluwa@gmail.com>
2022-12-24 17:46:17 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
