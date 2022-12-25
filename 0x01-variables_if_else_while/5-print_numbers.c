939c4e47146612fd741580a714101eb0f2cefcb7
Ife <ife.ifeoluwa@gmail.com>
2022-12-24 17:18:48 -0800

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base_ten;
+
+	for (base_ten = 0; base_ten < 10; base_ten++)
+	{
+		printf("%d", base_ten);
+	}
+	printf("\n");
+	return (0);
+}
