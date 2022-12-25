728fc03d050cb2cad61365ec65d77646965a4e52
Ife <ife.ifeoluwa@gmail.com>
2022-12-25 06:54:27 -0800

    prints all combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digits numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (a != b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
