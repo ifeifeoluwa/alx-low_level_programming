e14ff88b779f5df8d198fcba5b08a4c1d657a99e
Ife <ife.ifeoluwa@gmail.com>
2022-12-24 16:25:15 -0800

    prints alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sam;
+
+	for (sam = 'a'; sam <= 'z'; sam++)
+	{
+		putchar(sam);
+	}
+	putchar('\n');
+	return (0);
+}
