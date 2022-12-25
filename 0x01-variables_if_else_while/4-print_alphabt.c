8ea2f6e2b971df0ac2db03fc727e550e70ac2eb0
Ife <ife.ifeoluwa@gmail.com>
2022-12-24 17:04:47 -0800

    prints alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'q' && alph != 'e')
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
