158e7fa770c22c5f30418db677c3c97c7be4d420
Ife <ife.ifeoluwa@gmail.com>
2022-12-25 08:48:57 -0800

    prints combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c, d = 0;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
