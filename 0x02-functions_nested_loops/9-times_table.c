e7b72bf8017cb9aed39214b0cf535a69da718559
Ife <ife.ifeoluwa@gmail.com>
2022-12-27 17:07:06 -0800

    prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - p4ints the 9 times table
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9, a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+		_putchar(c);
+		_putchar(',');
+		_putchar(' ');
+		}
+	_putchar('\n');
+	}
+}
