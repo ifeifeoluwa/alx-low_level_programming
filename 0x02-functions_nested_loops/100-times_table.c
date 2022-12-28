8050a8e2ca6601e6cf5bfbfcecc90147332ec46a
Ife <ife.ifeoluwa@gmail.com>
2022-12-27 18:32:28 -0800

    prints n times table starting from 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 			{
 				_putchar('0' + c / 100);
 				_putchar('0' + (c - 100) / 10);
-				_putchar('0' + c % 10)
+				_putchar('0' + c % 10);
 			}
 			if (b < n)
 			{
