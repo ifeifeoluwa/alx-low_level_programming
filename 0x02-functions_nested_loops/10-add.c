bf44474b1be5ff0ffee87c49a9ff1b24bc6a4f1f
Ife <ife.ifeoluwa@gmail.com>
2022-12-27 17:32:37 -0800

    finds sum of 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints sum if 2 integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+	return (a + b);
