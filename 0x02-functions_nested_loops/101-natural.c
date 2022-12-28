5fb02f46bd1e35a4b840f43e27c402ae3b199faf
Ife <ife.ifeoluwa@gmail.com>
2022-12-27 18:48:54 -0800

    prints tge sum of all multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of all multiples if 3 and 5 that is less than 1024
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a = 0, b = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0 || c % 5 == 0)
+		{
+			b += a;
+		}
+		a++;
+	}
+	printf("%i\n", c);
+	return (0);
+}
