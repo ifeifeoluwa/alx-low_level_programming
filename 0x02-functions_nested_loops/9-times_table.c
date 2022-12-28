ddfebbc16840b24be0e99f62b6ec05ef184cbe71
Ife <ife.ifeoluwa@gmail.com>
2022-12-27 17:12:06 -0800

    prints 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 48; a <= 57; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 48; b <= 57; b++)
 		{
 			c = a * b;
 		_putchar(c);
