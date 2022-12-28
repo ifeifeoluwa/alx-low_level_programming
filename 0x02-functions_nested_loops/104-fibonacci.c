120063ec252c19886eebb3464696fd52adb65e52
Ife <ife.ifeoluwa@gmail.com>
2022-12-28 15:55:14 -0800

    sequence of first 98 fib9nacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints 98 fibonacci numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+	unsigned long int b = 1, c = 2, d, e, f, g;
+
+	printf("%lu", b);
+
+	for (a = 1; a < 91; i++)
+	{
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b;
+	}
+
+	d = b / 1000000000;
+	e = b % 1000000000;
+	f = c / 1000000000;
+	g = c % 1000000000;
+
+	for (a = 92; a < 99; ++i)
+	{
+		printf(", %lu", f + (g / 1000000000));
+		printf("%lu", g % 1000000000);
+		f = f + d;
+		d = f - d;
+		g = g + e;
+		e = g - e;
+	}
+	printf("\n");
+	return (0);
+}
