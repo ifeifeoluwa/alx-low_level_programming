daa989bfcfeed141509bb3293b34ab36aeb6b27a
Ife <ife.ifeoluwa@gmail.com>
2023-01-01 13:58:45 -0800

    prints 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14 ten times
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j = 1;
+
+	while (j <= 10)
+	{
+		for (i = 48; i <= 62; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
