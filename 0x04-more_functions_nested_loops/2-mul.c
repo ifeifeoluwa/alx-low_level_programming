d1c74a60ccc16c7e350a0c1f828b60078136783e
Ife <ife.ifeoluwa@gmail.com>
2023-01-01 13:11:54 -0800

    multiplies 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: results of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
