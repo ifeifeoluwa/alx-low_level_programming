c3d3d7f8edd2dc52dfd91b55c8f15a60a641d143
Ife <ife.ifeoluwa@gmail.com>
2023-01-01 14:36:27 -0800

    prints 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,11 +15,8 @@ void more_numbers(void)
 		for (i = 48; i <= 62; i++)
 		{
 			if (i <= 57)
-				_putchar(i);
-			else
-			{
 				_putchar(i / 58);
-				_putchar(i % 58);
+			_putchar(i % 58);
 			}
 		}
 		_putchar('\n');
