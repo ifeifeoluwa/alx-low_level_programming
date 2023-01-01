ea73a7d954486ca31099cfecd3938f0f4df6722c
Ife <ife.ifeoluwa@gmail.com>
2023-01-01 12:08:32 -0800

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks uppercas3
+ * @c: parameter
+ * Return: 0 if sucessful
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
