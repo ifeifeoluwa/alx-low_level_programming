642269eb55fdf6d725f21f6e60639e18d48a6d58
Ife <ife.ifeoluwa@gmail.com>
2023-01-01 18:22:15 -0800

    prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: parameter
+ */
+
+void print_number(int n)
+{
+	unsigned int i, j, k;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+		i = n;
+	j = i;
+
+	while (j > 9)
+	{
+		j /= 10;
+		k *= 10;
+	}
+	for (k = 1; k >= 1; k /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
+}
