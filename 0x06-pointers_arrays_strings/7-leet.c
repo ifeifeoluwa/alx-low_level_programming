74e0be138459688008877c1a38ce2f021552a620
Ife <ife.ifeoluwa@gmail.com>
2023-01-02 10:50:11 -0800

    this function encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - this function encodes a string into 1337
+ * @s: pointer to string
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int sc = 0, lc;
+	char ll[] = "aAeEoOtTlL";
+	char ln[] = "4433007711";
+
+	while (s[sc] != '\0')
+	{
+		lc = 0;
+
+		while (lc < 10)
+		{
+			if (ll[lc] == s[lc])
+				s[sc] = ln[lc];
+			lc++;
+		}
+		sc++;
+	}
+	return (s);
+}
