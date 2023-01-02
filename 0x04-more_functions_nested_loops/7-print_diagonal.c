4c0f5fda1752355cbf18b4488b03c96dd5fe4a33
Ife <ife.ifeoluwa@gmail.com>
2023-01-01 16:37:12 -0800

    prints diagonal lines on th3 screen

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal lines on the screen
+ * @n: number of times to print \
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
