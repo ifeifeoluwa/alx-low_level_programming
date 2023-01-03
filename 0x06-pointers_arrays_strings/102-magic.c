3fe47a9db998b969ad735b06e401167ea58eefa6
Ife <ife.ifeoluwa@gmail.com>
2023-01-03 04:39:40 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,10 +3,10 @@
 int main(void)
 {
   int n;
-  int a[5];
+  int b[5];
   int *p;
-  
-  a[2] = 1024;
+
+  b[2] = 1024;
   p = &n;
   /*
    * write your line of code here...
@@ -18,6 +18,6 @@ int main(void)
    */
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
+  printf("b[2] = %d\n", b[2]);
   return (0);
 }
