a55519e99b3b8f7a595882f140477b28494b653c
Ife <ife.ifeoluwa@gmail.com>
2023-01-03 00:49:30 -0800

    seaches a string for a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+define NULL 0
+
+/**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: string
+ * @accept: target
+ * Return: s when found and NULL when not found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a] != '\0')
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				s = &s[a];
+				return (s);
+			}
+		}
+		a++;
+	}
+	return (NULL);
+}
