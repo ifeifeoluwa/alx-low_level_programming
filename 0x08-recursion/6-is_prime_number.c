4b4067c72eac996a903fd844fbf3a2addbfbcfb0
Ife <ife.ifeoluwa@gmail.com>
2023-01-19 02:10:05 -0800

    check if number is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * find_prime - recursion loop
+ * @n: number
+ * @i: number to iterate
+ * Return: return 1 or 0
+ */
+
+int find_prime(int n, int i)
+{
+	if (i == n - 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+
+	if (n % i != 0)
+		return (find_prime(n, i + 1));
+
+	return (0);
+}
+
+/**
+ * is_prime_number - check if number is a p4ime number
+ * @num: number
+ * Return: returns 1 or 0
+ */
+
+int is_prime_number(int num)
+{
+	int i = 2;
+
+	if (num < 2)
+		return (0);
+
+	if (num == 2)
+		return (1);
+
+	return (find_prime(n, i));
+}
