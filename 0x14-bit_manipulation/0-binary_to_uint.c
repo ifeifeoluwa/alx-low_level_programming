2a417f0df6397a0aa186bd243a83c7cdd7bc7ccc
Ife <ife.ifeoluwa@gmail.com>
2023-02-02 16:02:16 -0800

    converts a binary to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @bn: binary number
+ * Return: the unsigned int or 0 when the conversion fails
+ */
+
+unsigned int binary_to_uint(const char *bn)
+{
+	unsigned int uint, pl;
+	int n;
+
+	if (!bn)
+		return (0);
+
+	for (n = 0; bn[n]; n++)
+	n--;
+
+	for (pl = 1, uint = 0; n >= 0; n--)
+	{
+		if (bn[n] == '0')
+			pl *= 2, continue;
+		else if (bn[n] == '1')
+			uint += pl, pl *= 2, continue;
+		return (0);
+	}
+	return (uint);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This folder contains codes on bits manipulation
