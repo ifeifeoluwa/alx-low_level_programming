ecf3db456ed9602e514daaa1aa10157375bab05b
Ife <ife.ifeoluwa@gmail.com>
2023-04-06 08:50:14 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,46 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @bn: binary number
- * Return: the unsigned int or 0 when the conversion fails
- */
-
-unsigned int binary_to_uint(const char *bn)
+  * _stoi - converts chars to ints
+  * @c: char to convert
+  * Return: converted int
+  */
+unsigned int _stoi(char c)
 {
-	unsigned int uint, pl;
-	int n;
+	return ((unsigned int) c - '0');
+}
+/**
+  * _strlen - calculates the length of the string
+  * @s: input
+  * Return: length of string
+  */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
 
-	if (!bn)
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+  * binary_to_uint - converts a string of 1's and 0's to a decimal number
+  * @b: string to convert
+  * Return: unsigned decimal number
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int result, tmp, expo;
+
+	if (!b)
 		return (0);
-
-	for (n = 0; bn[n]; n++)
-	n--;
-
-	for (pl = 1, uint = 0; n >= 0; n--)
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
 	{
-		if (bn[n] == '0')
-		{
-			pl *= 2;
-			continue;
-		}
-		else if (bn[n] == '1')
-		{
-			uint += pl; 
-			pl *= 2; 
-			continue;
-		}
-		return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
 	}
-	return (uint);
+	return (result);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -10,4 +15,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif
+#endif /* MAIN_H */
